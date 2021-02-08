f8f5045612e75a5a8eeda4ff0ab871e8fbf351c2
Joshua Marcus <josh.m@resvu.com.au>
2021-02-08 17:20:03 +1030

    removed config

--- src/env/config.ts
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyBaaNXOsruNPyDrQpKEnw-S-BnvwKlTfR4",
-  authDomain: "comm-unstable.firebaseapp.com",
-  databaseURL: "https://comm-unstable.firebaseio.com",
-  projectId: "comm-unstable",
-  storageBucket: "comm-unstable.appspot.com",
-  messagingSenderId: "1061697142615",
-  appId: "1:1061697142615:web:23f9c7b10d97acfe"
-};
