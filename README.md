89835700b3fb9468f396375ed05ea35b38b47e05
Joshua Marcus <josh.m@resvu.com.au>
2021-03-03 17:09:41 +1030

    attachmetns working

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Outlook Add-In Firestore Sync
 
 
+# Development
+
+- Development server can be ```npm run dev-server```
--- src/taskpane/taskpane.ts
+++ src/taskpane/taskpane.ts
@@ -3,16 +3,23 @@ import "../../assets/icon-32.png";
 import "../../assets/icon-80.png";
 import firebase from "firebase/app";
 import "firebase/auth";
-import { firebaseConfig } from "../env/config";
-import * as md5 from 'md5'
+import "firebase/storage";
+import { config } from "../env/config";
+import * as md5 from "md5";
+
+interface Attachment {
+  contentType: string;
+  name: string;
+  content: string;
+  downloadURL?: string;
+}
 
 /* global document, Office */
 
-
 Office.onReady(info => {
   if (info.host === Office.HostType.Outlook) {
-    firebase.initializeApp(firebaseConfig);
-    firebase.auth().onAuthStateChanged((user) => {
+    firebase.initializeApp(config.firebaseConfig);
+    firebase.auth().onAuthStateChanged(user => {
       if (user != null) {
         document.getElementById("auth").style.display = "unset";
         document.getElementById("no-auth").style.display = "none";
@@ -27,9 +34,6 @@ Office.onReady(info => {
   }
 });
 
-
-
-
 async function login() {
   try {
     await firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL);
@@ -40,107 +44,101 @@ async function login() {
     document.getElementById("login-result").innerHTML = e.message;
     document.getElementById("login-result").style.color = "#f44336";
     console.log(e);
-
   }
 }
-
 
 async function syncEmail() {
   try {
     // Recheck Auth
     const isLoggedIn = firebase.auth().currentUser;
-    if(!isLoggedIn) {
+    if (!isLoggedIn) {
       const noAuthMessage: Office.NotificationMessageDetails = {
+        type: Office.MailboxEnums.ItemNotificationMessageType.InformationalMessage,
+        message: "Please login to use this function.",
+        icon: "Icon.80x80",
+        persistent: false
+      };
+      Office.context.mailbox.item.notificationMessages.replaceAsync("action", noAuthMessage);
+    }
+    const mailRecord = Office.context.mailbox.item;
+
+    const storageRef = firebase.storage().ref();
+
+    const idString: string = md5(mailRecord.itemId);
+    const body = await getMailBody(Office.CoercionType.Html);
+    let attachments = await getMailAttachments();
+    attachments = await Promise.all(
+      attachments.map(async attachment => {
+        const imageRef = storageRef.child(config.attachmentStoragePath + attachment.name);
+        const storageObj = await imageRef.putString(
+          `data:${attachment.contentType};base64, ` + attachment.content,
+          "data_url"
+        );
+        const downloadURL = await storageObj.ref.getDownloadURL();
+        attachment.downloadURL = downloadURL;
+        return attachment;
+      })
+    );
+    const dbMailObj = {
+      id: idString,
+      body,
+      subject: mailRecord.subject,
+      from: { name: mailRecord.from.displayName, email: mailRecord.from.emailAddress },
+      attachments: attachments,
+      userId: isLoggedIn.uid
+    };
+    console.log({ dbMailObj });
+    await firebase
+      .firestore()
+      .collection(config.emailCollectionPath)
+      .add(dbMailObj);
+    const completed: Office.NotificationMessageDetails = {
       type: Office.MailboxEnums.ItemNotificationMessageType.InformationalMessage,
-      message: "Please login to use this function.",
+      message: "Your email has been synced!",
       icon: "Icon.80x80",
       persistent: false
     };
-    Office.context.mailbox.item.notificationMessages.replaceAsync("action", noAuthMessage);
-    }
-
-    // var bodyResult;
-
-    const mailRecord = Office.context.mailbox.item;
-    // mailRecord.getAttachmentsAsync(async function(attachment) {
-    //   attachment.value.map(a => a.url)
-    // })
-    // const attachemnets = mailRecord.attachments;
-    // attachemnets.map(at => at)
-      const idString: string = md5(mailRecord.itemId);
-      const body = await getMailBody(Office.CoercionType.Html);
-      const attachments = await getMailAttachments();
-      console.log(attachments);
-      const dbMailObj = {
-        id: idString,
-        body,
-        subject: mailRecord.subject,
-        from: {name: mailRecord.from.displayName, email: mailRecord.from.emailAddress}
-      }
-
-      console.log(dbMailObj)
-  //   var body = Office.context.mailbox.item.body;
-  //   body.getAsync(Office.CoercionType.Html, async function (asyncResult) {
-  //     if (asyncResult.status !== Office.AsyncResultStatus.Succeeded) {
-  //         bodyResult = "";
-  //     } else {
-  //         bodyResult = `${asyncResult.value}`;
-  //         await firebase.firestore().collection('users').doc(authRecord.user.uid).collection('synced-emails').add({
-  //           from: {name: mailRecord.from.displayName, email: mailRecord.from.emailAddress},
-  //           userId: authRecord.user.uid,
-  //           subject: mailRecord.subject,
-  //           body: bodyResult,
-  //           mailbox
-  //           id: itemId
-  //         }, {merge: true});
-  //         const completed: Office.NotificationMessageDetails = {
-  //           type: Office.MailboxEnums.ItemNotificationMessageType.InformationalMessage,
-  //           message: "You email has been synced!",
-  //           icon: "Icon.80x80",
-  //           persistent: false
-  //         };
-  //         Office.context.mailbox.item.notificationMessages.replaceAsync("action", completed);
-
-  //     }
-  //  });
-
+    Office.context.mailbox.item.notificationMessages.replaceAsync("action", completed);
   } catch (e) {
-    console.log('Something went wrong', e)
-    // console.log(e);
-    // event.completed();
-
+    console.log(e);
   }
-
 }
 
 async function getMailBody(coercionType: Office.CoercionType) {
   return new Promise((resolve, reject) => {
     const body = Office.context.mailbox.item.body;
-    body.getAsync(coercionType, function (asyncResult) {
-      if (asyncResult.status !== Office.AsyncResultStatus.Succeeded) {
-          reject()
-      } else {
-        resolve(asyncResult.value)
-
-      }
-   });
-  })
-}
-
-
-async function getMailAttachments(): Promise<string> {
-  return new Promise((resolve, reject) => {
-    const attachments = Office.context.mailbox.item.attachments;
-    console.log({attachments});
-    Office.context.mailbox.item.getAttachmentContentAsync(attachments[0].id, function (asyncResult) {
+    body.getAsync(coercionType, function(asyncResult) {
       if (asyncResult.status !== Office.AsyncResultStatus.Succeeded) {
         reject();
       } else {
-        console.log(asyncResult.value.content);
-        resolve(asyncResult.value.content)
+        resolve(asyncResult.value);
       }
     });
   });
+}
+
+async function getMailAttachments(): Promise<Attachment[]> {
+  const attachments = Office.context.mailbox.item.attachments;
+  const attachmentsNotInline = attachments.filter(attachment => !attachment.isInline);
+  const attachmentContent = await Promise.all(
+    attachmentsNotInline.map(async attachment => {
+      return await new Promise((resolve, reject) => {
+        Office.context.mailbox.item.getAttachmentContentAsync(attachment.id, function(asyncResult) {
+          if (asyncResult.status !== Office.AsyncResultStatus.Succeeded) {
+            reject();
+          } else {
+            resolve({
+              content: asyncResult.value.content,
+              contentType: attachment.contentType,
+              name: attachment.name
+            } as Attachment);
+          }
+        });
+      });
+    })
+  );
+
+  return attachmentContent as Attachment[];
 }
 
 async function logout() {
